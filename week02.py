cdb9ab4d188b8857ead823d1f441f9f3463b4796
inhadeepblue <deepblue@inha.ac.kr>
2025-03-11 20:16:50 +0900

    week02 kiosk v0.3

--- week02.py
+++ week02.py
@@ -1,10 +1,11 @@
 # 1) Ice Americano : 2000  2) Cafe Latte : 3000
+prices = [2000, 3000]
 while True:
-    menu = input("1) Ice Americano  2) Cafe Latte  3) Exit : ")
+    menu = input(f"1) Ice Americano {prices[0]}won  2) Cafe Latte {prices[1]}won  3) Exit : ")
     if menu == "1":
-        print("Ice Americano ordered. Price : 2000 won")
+        print(f"Ice Americano ordered. Price : {prices[0]}won")
     elif menu == "2":
-        print("Cafe Latte ordered. Price : 3000 won")
+        print(f"Cafe Latte ordered. Price : {prices[1]}won")
     elif menu == "3":
         print("Finish order~")
         break
